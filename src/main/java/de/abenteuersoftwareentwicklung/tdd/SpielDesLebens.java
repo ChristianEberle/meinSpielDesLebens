7a6270a6a2090084fbde63a167469300a9ca3ebc
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 14:31:44 +0100

    After third blue phase

--- src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
+++ src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
@@ -2,9 +2,11 @@ package de.abenteuersoftwareentwicklung.tdd;
 
 public class SpielDesLebens {
 
+    private static final int GUTE_LEBENSBEDINGUNG = 3;
+
     public boolean neuerZustand(boolean aktuellerZustand, boolean[] nachbarn) {
         int lebendeNachbarn = anzahlLebenderZellen(nachbarn);
-        return lebendeNachbarn == 3;
+        return lebendeNachbarn == GUTE_LEBENSBEDINGUNG;
     }
 
     private int anzahlLebenderZellen(boolean[] nachbarn) {
