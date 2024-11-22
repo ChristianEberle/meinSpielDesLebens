1a377d312a397fdd37118def4dbca1eeed7caeea
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 14:14:51 +0100

    after new tests but all green

--- src/test/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebensTest.java
+++ src/test/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebensTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class SpielDesLebensTest {
 
     @Test
-    public void toteZelle_genau3LebendeNachbarn_geboren() {
+    public void toteZelle_genau3LebendeNachbarn_wirdGeboren() {
 
         SpielDesLebens spiel = new SpielDesLebens();
 
@@ -24,6 +24,24 @@ public class SpielDesLebensTest {
     }
 
     @Test
+    public void toteZelle_genau3LebendeNachbarn_wirdGeboren_andereReihenfolge() {
+
+        SpielDesLebens spiel = new SpielDesLebens();
+
+        final boolean toteZelle = false;
+
+        boolean[] nachbarn = {  true,   false,   false, //
+                                false,          true, //
+                                false,  true,  false
+        };
+
+        boolean neuerZustand = spiel.neuerZustand(toteZelle, nachbarn);
+
+        assertTrue(neuerZustand);
+
+    }
+
+    @Test
     public void toteZelle_weniger3LebendeNachbarn_bleibtTot() {
 
         SpielDesLebens spiel = new SpielDesLebens();
@@ -33,6 +51,23 @@ public class SpielDesLebensTest {
         boolean[] nachbarn = {  false,  true,   true, //
                                 false,          false, //
                                 false,  false,  false //
+        };
+
+        boolean neuerZustand = spiel.neuerZustand(toteZelle, nachbarn);
+
+        assertFalse(neuerZustand);
+    }
+
+    @Test
+    public void toteZelle_weniger3LebendeNachbarn_bleibt_Tot_keineLebendeNachbarn() {
+
+        SpielDesLebens spiel = new SpielDesLebens();
+
+        final boolean toteZelle = false;
+
+        boolean[] nachbarn = {  false,  false,  false,
+                                false,          false,
+                                false,  false,  false
         };
 
         boolean neuerZustand = spiel.neuerZustand(toteZelle, nachbarn);
