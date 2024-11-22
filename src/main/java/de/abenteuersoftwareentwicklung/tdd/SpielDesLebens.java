a017df52427be5f0b9e99460c6d9537bd263e679
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 15:13:15 +0100

    After fifth blue phase

--- src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
+++ src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
@@ -4,18 +4,25 @@ public class SpielDesLebens {
 
     private static final int GUTE_LEBENSBEDINGUNG = 3;
 
-    public boolean neuerZustand(boolean aktuellerZustand, boolean[] nachbarn) {
-        if(aktuellerZustand) {
-            return anzahlLebenderZellen(nachbarn) >= 2;
-        }
-        int lebendeNachbarn = anzahlLebenderZellen(nachbarn);
-        return lebendeNachbarn == GUTE_LEBENSBEDINGUNG;
+    public boolean neuerZustand(boolean aktuelleZelleLebt, boolean[] nachbarn) {
+        if (aktuelleZelleLebt)
+            return neuerZustandLebenderZelle(nachbarn);
+        else
+            return neuerZustandToterZelle(nachbarn);
+    }
+
+    private boolean neuerZustandLebenderZelle(boolean[] nachbarn) {
+        return anzahlLebenderZellen(nachbarn) >= 2;
+    }
+
+    private boolean neuerZustandToterZelle(boolean[] nachbarn) {
+        return anzahlLebenderZellen(nachbarn) == GUTE_LEBENSBEDINGUNG;
     }
 
     private int anzahlLebenderZellen(boolean[] nachbarn) {
         int anzahl = 0;
         for (boolean zustand : nachbarn) {
-            if(zustand) anzahl++;
+            if (zustand) anzahl++;
         }
         return anzahl;
     }
