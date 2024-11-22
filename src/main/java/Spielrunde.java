bc7072d587ae89d8a78bd576cb7748e8a820fa9b
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 19:07:08 +0100

    After second green phase for game round

--- src/main/java/Spielrunde.java
+++ src/main/java/Spielrunde.java
@@ -3,10 +3,21 @@ import de.abenteuersoftwareentwicklung.tdd.Spielbrett;
 
 public class Spielrunde {
 
+    private Lebensfunke funke;
+
     public Spielrunde(Lebensfunke funke) {
+        this.funke = funke;
     }
 
     public Spielbrett naechsteSituation(Spielbrett aktuelleSituation) {
-        return aktuelleSituation;
+        boolean[][] neueZellen = new boolean[4][4];
+        for (int x = 0; x < neueZellen.length; x++) {
+            for (int y = 0; y < neueZellen[x].length; y++) {
+                boolean aktuelleZelle = aktuelleSituation.statusDerZelle(x, y);
+                boolean[] nachbarn = aktuelleSituation.getNachbarnDerZelle(x, y);
+                neueZellen[x][y] = funke.neuerZustand(aktuelleZelle, nachbarn);
+            }
+        }
+        return new Spielbrett(neueZellen);
     }
 }
