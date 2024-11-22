18f6461e9337b87df671aafbfb4ec08c14379a1c
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 18:58:18 +0100

    After second red phase for game round

--- src/test/java/SpielrundeTest.java
+++ src/test/java/SpielrundeTest.java
@@ -53,4 +53,28 @@ public class SpielrundeTest {
         Spielbrett erwarteteSituation = new Spielbrett(erwarteteZellen);
         assertEquals(erwarteteSituation, neueSituation);
     }
+
+    @Test
+    public void blinkerBlinkt() {
+        Lebensfunke funke = new Lebensfunke();
+        Spielrunde runde = new Spielrunde(funke);
+        boolean[][] zellen = {
+                {false, false, false, false},
+                {false, true,  true,  true},
+                {false, false, false, false},
+                {false, false, false, false},
+        };
+        Spielbrett aktuelleSituation = new Spielbrett(zellen);
+
+        Spielbrett neueSituation = runde.naechsteSituation(aktuelleSituation);
+
+        boolean [][] erwarteteZellen = {
+                {false, false, true,  false},
+                {false, false, true,  false},
+                {false, false, true,  false},
+                {false, false, false, false},
+        };
+        Spielbrett erwarteteSituation = new Spielbrett(erwarteteZellen);
+        assertEquals(erwarteteSituation, neueSituation);
+    }
 }
