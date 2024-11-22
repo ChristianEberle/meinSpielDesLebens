6b5be2e1ba01ec55eba0e51fe643c537f3a5584d
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 15:59:36 +0100

    After second red phase for gameboard

--- src/test/java/de/abenteuersoftwareentwicklung/tdd/SpielbrettTest.java
+++ src/test/java/de/abenteuersoftwareentwicklung/tdd/SpielbrettTest.java
@@ -17,7 +17,19 @@ public class SpielbrettTest {
         Spielbrett spielbrett = new Spielbrett(zellen);
 
         assertTrue(spielbrett.statusDerZelle(1,1));
+    }
 
+    @Test
+    public void gibStatusEinerAnderenZelleZurueck() {
+        boolean[][] zellen = {
+                {false, false, false},
+                {false, true,  false},
+                {false, false, false}
+        };
+
+        Spielbrett spielbrett = new Spielbrett(zellen);
+
+        assertFalse(spielbrett.statusDerZelle(1,2));
     }
 
 }
