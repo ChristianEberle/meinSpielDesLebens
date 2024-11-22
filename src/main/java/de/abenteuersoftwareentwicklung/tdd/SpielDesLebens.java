d27db560001c364b2f05daebe507aad11fdcdf6a
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 14:51:01 +0100

    After fourth green phase

--- src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
+++ src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
@@ -5,6 +5,9 @@ public class SpielDesLebens {
     private static final int GUTE_LEBENSBEDINGUNG = 3;
 
     public boolean neuerZustand(boolean aktuellerZustand, boolean[] nachbarn) {
+        if(aktuellerZustand) {
+            return anzahlLebenderZellen(nachbarn) == 2;
+        }
         int lebendeNachbarn = anzahlLebenderZellen(nachbarn);
         return lebendeNachbarn == GUTE_LEBENSBEDINGUNG;
     }
