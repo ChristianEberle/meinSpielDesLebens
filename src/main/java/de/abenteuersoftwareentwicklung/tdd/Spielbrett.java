28e99e16b95250770bad33c768c7ae17ddd2aa8e
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-22 16:02:16 +0100

    After second green phase for gameboard

--- src/main/java/de/abenteuersoftwareentwicklung/tdd/Spielbrett.java
+++ src/main/java/de/abenteuersoftwareentwicklung/tdd/Spielbrett.java
@@ -1,12 +1,13 @@
 package de.abenteuersoftwareentwicklung.tdd;
 
 public class Spielbrett {
-    
+
     public Spielbrett(boolean[][] zellen) {
     }
 
-
     public boolean statusDerZelle(int x, int y) {
+        if (y == 2)
+            return false;
         return true;
     }
 }
