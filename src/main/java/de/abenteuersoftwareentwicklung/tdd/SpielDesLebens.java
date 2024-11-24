f88fde34e31c34778d35e1277c40c1b930241509
Christian Eberle <christian.eberle.78@gmx.de>
2024-11-24 14:48:32 +0100

    After first green phase for SpielDesLebens

--- src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
+++ src/main/java/de/abenteuersoftwareentwicklung/tdd/SpielDesLebens.java
@@ -1,10 +1,19 @@
 package de.abenteuersoftwareentwicklung.tdd;
 
 public class SpielDesLebens {
+
+    private Spielbrett situation;
+    private Ausgabe ausgabe;
+
     public SpielDesLebens(Spielbrett anfangsSituation, Ausgabe ausgabe) {
+        this.situation = anfangsSituation;
+        this. ausgabe = ausgabe;
     }
 
     public void go() {
+        ausgabe.zeige(situation);
+        situation = situation.naechsteSituation();
+        ausgabe.zeige(situation);
     }
 
 }
